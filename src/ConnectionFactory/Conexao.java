2c421f297a51a0484f2f537092eee925c3c902be
Elison Abreu <elisonwabreu@gmail.com>
2014-04-23 21:49:29 -0300

    Teste

--- src/ConnectionFactory/Conexao.java
+++ src/ConnectionFactory/Conexao.java
@@ -3,7 +3,6 @@ package ConnectionFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
