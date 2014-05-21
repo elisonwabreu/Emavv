c15a0fe00a8f35f495aaa0d975a710a37e2d1e91
Elison Abreu <elisonwabreu@gmail.com>
2014-05-21 20:14:12 -0300

    a

--- src/org/entities/classes/tb_matriculas.java
+++ src/org/entities/classes/tb_matriculas.java
@@ -3,14 +3,22 @@ package org.entities.classes;
 import java.io.Serializable;
 import java.sql.Date;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class tb_matriculas implements Serializable {
     
+    @Id
     public int codigo;
     public int aluno;
     public Date dataMatricula;
     public int usuario;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+   
 
    
 }
